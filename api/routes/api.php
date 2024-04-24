ec9d2733fb5c10c8f09924682c100d185ba3d68b
Asjid Ali <asjidale@gmail.com>
2024-04-24 11:37:37 +0500

    Disabled the offer and ProductOffers functionalities

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\OfferController;
+// use App\Http\Controllers\OfferController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductOfferController;
-use App\Http\Controllers\UserController;
+// use App\Http\Controllers\ProductOfferController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::any('auth/login', [AuthController::class, 'login']);
@@ -20,13 +20,13 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::resource('order', OrderController::class);
     Route::resource('announcement', AnnouncementController::class);
     Route::resource('cart', CartController::class);
-    Route::resource('offer', OfferController::class);
+    // Route::resource('offer', OfferController::class);
     Route::resource('product', ProductController::class);
-    Route::resource('productoffer', ProductOfferController::class);
+    // Route::resource('productoffer', ProductOfferController::class);
 
     Route::get('dashboard', [DashboardController::class, 'dashboard']);
     Route::get('myCart', [CartController::class, 'myCart']);
-    Route::get('alloffer', [OfferController::class, 'all']);
+    // Route::get('alloffer', [OfferController::class, 'all']);
     Route::get('allprodut', [ProductController::class, 'all']);
     Route::get('allprodut', [ProductController::class, 'all']);
     Route::get('myOrders', [OrderController::class, 'myOrders']);
