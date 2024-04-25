16f71d328494d64d97f9b7490ca464226b9a147b
Asjid Ali <asjidale@gmail.com>
2024-04-25 18:20:03 +0500

    updated the permissions and along api middleware from web middleware

--- api/database/migrations/2024_04_17_160726_add__roles_and__permissions.php
+++ api/database/migrations/2024_04_17_160726_add__roles_and__permissions.php
@@ -12,35 +12,47 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $executive = Role::create(['name' => 'Executive']);
-        Role::create(['name' => 'Farmer']);
-        Role::create(['name' => 'Exporter']);
-        Role::create(['name' => 'Logicstics']);
-        Role::create(['name' => 'Guest']);
+        $executive = Role::create(['guard_name' => 'api', 'name' => 'Executive']);
+        Role::create(['guard_name' => 'api', 'name' => 'Farmer']);
+        Role::create(['guard_name' => 'api', 'name' => 'Exporter']);
+        Role::create(['guard_name' => 'api', 'name' => 'Logicstics']);
+        Role::create(['guard_name' => 'api', 'name' => 'Guest']);
 
-        Permission::create(['name' => 'View User'])->assignRole($executive);
-        Permission::create(['name' => 'Create User'])->assignRole($executive);
-        Permission::create(['name' => 'Update User'])->assignRole($executive);
-        Permission::create(['name' => 'Delete User'])->assignRole($executive);
-        Permission::create(['name' => 'View Roles'])->assignRole($executive);
-        Permission::create(['name' => 'View Permissions'])->assignRole($executive);
-        Permission::create(['name' => 'Update Permissions'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'View User'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Activate User'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Deactivate User'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'View Dashboard'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Create User'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Update User'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Fulfil Order'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Delete User'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'View Roles'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'View Permissions'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Update Permissions'])->assignRole($executive);
         // Permission::create(['name' => 'Update Roles'])->assignRole($executive);
-        Permission::create(['name' => 'View Products'])->assignRole($executive);
-        Permission::create(['name' => 'Update Products'])->assignRole($executive);
-        Permission::create(['name' => 'Create Offers'])->assignRole($executive);
-        Permission::create(['name' => 'View Offers'])->assignRole($executive);
-        Permission::create(['name' => 'Update Offers'])->assignRole($executive);
-        Permission::create(['name' => 'Delete Offers'])->assignRole($executive);
-        Permission::create(['name' => 'Insert Announcement'])->assignRole($executive);
-        Permission::create(['name' => 'Update Announcement'])->assignRole($executive);
-        Permission::create(['name' => 'View Announcement'])->assignRole($executive);
-        Permission::create(['name' => 'Delete Announcement'])->assignRole($executive);
-        Permission::create(['name' => 'View Carts'])->assignRole($executive);
-        Permission::create(['name' => 'Delete Order'])->assignRole($executive);
-        Permission::create(['name' => 'View Order'])->assignRole($executive);
-        Permission::create(['name' => 'Insert Order'])->assignRole($executive);
-        Permission::create(['name' => 'Update Order Status'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Update Products'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Create Products'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Create Offers'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'View Offers'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Update Offers'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Delete Offers'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Insert Announcement'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Update Announcement'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'View Announcement'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Delete Announcement'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Delete Order'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'View Order'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Insert Order'])->assignRole($executive);
+        Permission::create(['guard_name' => 'api', 'name' => 'Update Order Status'])->assignRole($executive);
+
+        $user = User::create([
+            "username" => "asjidale",
+            "password" => '$2y$12$GO78H5Nk7RhWVi18uizIsOGsi9dFy7QBrbPAfQg.d74Zb..Z59FLO',
+            "email" => "asjidale@gmail.com",
+            "name" => 'Asjid Ali',
+        ]);
+
+        $user->assignRole($executive);
 
     }
 
