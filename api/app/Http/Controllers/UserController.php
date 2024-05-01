5ec2ff14c7893acca73169d868bc691805a54684
Asjid Ali <asjidale@gmail.com>
2024-05-01 17:28:08 +0500

    fix: password change missing alert

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends BaseController
 {
